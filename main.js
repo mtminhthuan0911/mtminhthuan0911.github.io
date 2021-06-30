4bb775fe64b8c987282cb9c92048a466d11a5bc6
thuanvm <thuanvm@nvpcloud.com>
2021-06-30 20:37:17 +0700

    Upload site map

--- main.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById("switch");
-const heading = document.getElementById("heading")
-checkbox.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-     heading.classList.toggle('white')
-});
