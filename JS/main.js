f6e7f42f3f3b338c98cf8c6dffa44713e724068c
thuanvm <thuanvm@nvpcloud.com>
2021-08-09 00:33:26 +0700

    Upload

--- JS/main.js
+++ JS/main.js
@@ -21,6 +21,45 @@ let intro = document.querySelector('.intro');
 // setTimeout (() => {
 //     document.getElementById("Twriter").play();
 // },8900)
-       
+
+let mangsanpham = [
+    {ID : 1,Tien : 400 , Hang : 'Sony'},
+    {ID : 2,Tien : 100 , Hang : 'Apple'},
+    {ID : 3,Tien : 200 , Hang : 'Keact'},
+    {ID : 4,Tien : 300 , Hang : 'Samsung'},
+];
+
+
+
+
+let mangsptheoten = mangsanpham.sort((sptieptheo , sp) => {
+    let tensptieptheo = sptieptheo.Hang.toLowerCase();
+    let tenSP = sp.Hang.toLocaleLowerCase();
+    if(tensptieptheo > tenSP)
+    {
+        return 1;
+    }
+    if(tensptieptheo < tenSP){
+        return -1;
+    }
+    return 1;
+});
+
+console.log('todo : ',mangsptheoten)
+
+
+// let mangsptheogia = mangsanpham.sort((sptieptheo, sp )=>{
+//     if((sptieptheo.Tien - sp.Tien > 0))
+//     {
+//         return -1;
+//     }
+//     else{
+//         return 1;
+//     }
+   
+// });
+
+// console.log('todo : ',mangsptheogia)
+
 
 
